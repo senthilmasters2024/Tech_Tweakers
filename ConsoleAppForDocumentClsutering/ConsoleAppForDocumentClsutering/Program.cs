42258c97c2d963962d8431434cb66afa18f4b880
Neethu-Ninan <neethu.ninan11@gmail.com>
2025-01-28 15:20:29 +0530

    satrted a module development to deal with document level embedding generation

---
+++ ConsoleAppForDocumentClsutering/ConsoleAppForDocumentClsutering/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"E:\SE PROJECT\documents\documents\Germany.txt";
+        int chunkSize = 1500;
+
+
+        {
+            string text = File.ReadAllText(filePath);
+            List<string> chunks = ChunkText(text, chunkSize);
+
+            Console.WriteLine($"Total Chunks Created: {chunks.Count}");
+
+
+            foreach (var chunk in chunks)
+            {
+                Console.WriteLine($"- {chunk}\n");
+            }
+        }
+    }
+{
+    if (!string.IsNullOrEmpty(chunk))
+            chunks.Add(chunk.Trim());
+
+        return chunks;
+    }
+}
