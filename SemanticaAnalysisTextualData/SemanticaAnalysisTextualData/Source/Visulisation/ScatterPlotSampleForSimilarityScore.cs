b019505d49f8de7251d18e98852afc5bdfb00248
senthilmasters2024 <senthilmasters2024@gmail.com>
2025-02-01 00:38:20 +0100

    TryingWithSomeSampleVisualisationInDotNet
    To Get Some Idea on how to plot our similarity scores with relevant and irrelevant scores

---
+++ SemanticaAnalysisTextualData/SemanticaAnalysisTextualData/Source/Visulisation/ScatterPlotSampleForSimilarityScore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Plotly.NET;
+using Plotly.NET.TraceObjects;
+
+class ScatterPlotSampleForSimilarityScore
+{
+    static void Main()
+    {
+        // Sample data (X-axis: document names, Y-axis: similarity scores)
+        var documentNames = new List<string>
+        {
+            "ProfileADevOps",
+            "ProfileBDataScience",
+            "ProfileCFullStackDeveloper"
+        };
+
+        var similarityScores = new List<double>
+        {
+            0.4479,
+            0.3328,
+            0.6276
+        };
+
+        // Create scatter plot
+        var scatterPlot = Chart2D.Chart.Scatter<string, double, string>(
+            x: documentNames,          // X-axis: Document names
+            y: similarityScores,       // Y-axis: Similarity scores
+            mode: StyleParam.Mode.Markers // Scatter plot mode
+        )
+        .WithMarker(Marker.init(Color: Color.fromString("blue"), Size: 10)) // Styling
+        .WithTitle("Document Similarity Scores")
+        .WithXAxisStyle(title: Title.init(Text: "Document Name")) // Fixing the error by using Title.init
+        .WithYAxisStyle(title: Title.init(Text: "Similarity Score")); // Fixing the error by using Title.init
+
+        // Display the chart
+        scatterPlot.Show();
+    }
+}
