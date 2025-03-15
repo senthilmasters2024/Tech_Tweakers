5ff045152e6415b6e85a7285be1fdd258a946680
Neethu-Ninan <neethu.ninan11@gmail.com>
2025-03-15 14:54:34 +0530

    Started working on unitests

---
+++ SemanticaAnalysisTextualData/TestPreprocessing/Test1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TestPreprocessing
+{
+    [TestClass]
+    public class TextPreprocessorTests
+    {
+        private TextPreprocessor _preprocessor;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _preprocessor = new TextPreprocessor();
+        }
+        [TestMethod]
+        public void PreprocessText_ShouldConvertToLowercase()
+        {
+            string input = "THE Goal of AI is to provide SOFTWARE that can reason on Input and explain on Output. ";
+            string expected = "the goal of AI is to provide software that can reason on input and explain on output.";
+            string result = _preprocessor.PreprocessText(input, TextDataType.Phrase);
+            Console.WriteLine($"Actual Output: {result}");
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void PreprocessText_ShouldNormalizeContractions()
+        {
+            string input = "I can't do this";
+            string expected = "i cannot do this";
+            string result = _preprocessor.PreprocessText(input, TextDataType.Phrase);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
