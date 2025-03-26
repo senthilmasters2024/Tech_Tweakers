fb39093c961e087d812bf82f03faa5bc0f1d99b2
senthilmasters2024 <senthilmasters2024@gmail.com>
2025-03-26 22:09:01 +0100

    Fix-SegregateClass

---
+++ SemanticAnalysisTextualData/SemanticAnalysisTextualData/Source/pojo/PhraseSimilarityInputCSV.cs
@@ -0,0 +1,29 @@
+﻿namespace SemanticAnalysisTextualData.Source.pojo
+{
+    /// <summary>
+    /// Represents the similarity between two phrases.
+    /// </summary>
+    public class PhraseSimilarityInputCSV
+    {
+        /// <summary>
+        /// Gets or sets the first phrase.
+        /// </summary>
+        public string? Phrase1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second phrase.
+        /// </summary>
+        public string? Phrase2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the domain of the phrases.
+        /// </summary>
+        public string? Domain { get; set; }
+
+        /// <summary>
+        /// Gets or sets the context of the phrases.
+        /// </summary>
+        public string? Context { get; set; }
+    }
+
+}
